6657904e74a1161cfc5042686586051828b52646
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-07-28 09:36:00 +0530

    correct db link

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-http-ca029-default-rtdb.asia-southeast1.firebasedatabase.app/meals"
+        "https://react-http-ca029-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
       );
 
       if (!response.ok) {
